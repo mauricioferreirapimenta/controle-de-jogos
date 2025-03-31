115a5a6ca564af99be00837deafd02acfd68f888
mauricioferreirapimenta <maumaupimenta@gmail.com>
2025-03-31 20:24:53 +0100

    Add files via upload

---
+++ controle_jogos_streamlit.py
@@ -0,0 +1,6 @@
+
+# Arquivo restaurado - favor substituir pelo conteúdo final completo da versão do app com:
+# - Edição via plataforma > console > jogo
+# - Botão de submit sempre presente no formulário de edição
+# - Modo seguro com login
+# - Exportar, importar, excluir jogos
