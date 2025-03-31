3bbfba78d39bfb17107d832823a9f98e4c359634
mauricioferreirapimenta <maumaupimenta@gmail.com>
2025-03-31 21:19:49 +0100

    Add files via upload

---
+++ controle_jogos_streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import os
+from datetime import date
+
+# -------------------------------
+# 🗃️ Adicionar novo jogo
+
+# -------------------------------
+# ✏️ Editar jogo
+
+# -------------------------------
+# 🗑️ Excluir jogo
+
+# -------------------------------
+# 📂 Navegação por Plataforma e Console
+
+# -------------------------------
+# 📋 Lista completa de jogos (ao final)
+
+# -------------------------------
+# Backup e Importação
+# -------------------------------
