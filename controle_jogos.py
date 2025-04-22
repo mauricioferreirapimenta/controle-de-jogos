78a59a4f014d7812de435c7681f89829738364a7
mauricioferreirapimenta <maumaupimenta@gmail.com>
2025-04-22 10:46:26 +0100

    controle_jogos.py

--- controle_jogos.py
+++ controle_jogos.py
@@ -108,10 +108,10 @@ if pagina == "✏️ Editar Jogo":
                 edicao = st.text_input("Edição", value=str(df.at[idx, "Edição"]))
                 condicao = st.text_area("Condição", value=str(df.at[idx, "Condição"]))
                 valor_data = df.at[idx, "Data de lançamento"]
-            if pd.isnull(valor_data) or valor_data == "":
-                valor_data = date.today()
-            else:
-                valor_data = pd.to_datetime(valor_data, errors="coerce").date()
+                if pd.isnull(valor_data) or valor_data == "":
+                    valor_data = date.today()
+                else:
+                    valor_data = pd.to_datetime(valor_data, errors="coerce").date()
                 data_lancamento = st.date_input("Data de lançamento", value=valor_data)
                 status = st.selectbox("Status", ["", "Jogando", "Zerado", "Parado", "Nunca Joguei"], index=["", "Jogando", "Zerado", "Parado", "Nunca Joguei"].index(str(df.at[idx, "Status"])) if str(df.at[idx, "Status"]) in ["", "Jogando", "Zerado", "Parado", "Nunca Joguei"] else 0)
                 nota = st.slider("Nota", 0, 10, int(df.at[idx, "Nota"]) if pd.notnull(df.at[idx, "Nota"]) else 0)
